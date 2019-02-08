277876b25dd87a30b4861ec12c2bb61d52129cb3
Certil Remy <certil@noukod.com>
2019-02-08 19:35:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ function BoardMember(name, homeState,training)
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-
+this.veto = function veto (){
+  console.log(No, I must disagree);
 }
+}
+
